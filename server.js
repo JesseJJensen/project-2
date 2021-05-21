bfb30efa9fea950c605add47bdd36475b22e4289
Jesse Jensen <81991370+JesseJJensen@users.noreply.github.com>
2021-05-21 15:06:43 -0400

    finished if-else for book cover img

--- server.js
+++ server.js
@@ -68,26 +68,26 @@ app.get('/results', isLoggedIn, (req, res) => {
   });
 });
 
-app.get('/results/:bookId', function(req, res) {
-  let bookId = req.query.bookId;
-  // console.log(input);
-  let googleBooksUrl = `https://www.googleapis.com/books/v1/volumes?${API_KEY}&q=${bookId}`;
-  // sets a variable equal to the API path + search terms
-  axios.get(googleBooksUrl).then(response => {
-    // uses axios to request the JSON data from the googleBooksURL and returns it as "response"
-    let searchReturn = response.data.items;
-    console.log('here is search return')
-    console.log(searchReturn)
-    //let bookData = [];
-    searchReturn.forEach( e => {
-        bookData.push(e)
-    });
-    console.log('here is book data .id')
-    console.log(bookData[0].id)
-    res.render('show', {book: bookData});
+// app.get('/results/:bookId', function(req, res) {
+//   let bookId = req.query.bookId;
+//   // console.log(input);
+//   let googleBooksUrl = `https://www.googleapis.com/books/v1/volumes?${API_KEY}&q=${bookId}`;
+//   // sets a variable equal to the API path + search terms
+//   axios.get(googleBooksUrl).then(response => {
+//     // uses axios to request the JSON data from the googleBooksURL and returns it as "response"
+//     let searchReturn = response.data.items;
+//     console.log('here is search return')
+//     console.log(searchReturn)
+//     //let bookData = [];
+//     searchReturn.forEach( e => {
+//         bookData.push(e)
+//     });
+//     console.log('here is book data .id')
+//     console.log(bookData[0].id)
+//     res.render('show', {book: bookData});
 
-  });
-});
+//   });
+// });
 
 // app.get('/results/:bookId', (req, res)=>{
 //   let bookId = req.query.bookId
